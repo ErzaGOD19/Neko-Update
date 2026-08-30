4531bf535295a46aa8a55b952e5f933f11437cd2
ANOMALI0x00 <anomali@DESK-H81m.tail329a0c.ts.net>
2026-08-29 21:51:11 -0400

    initial commints

--- neko-updater-tray/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_neko-updater-tray_FILE /home/anomali/STORAGE/Proyectos/Neko-Update/neko-updater-tray/build/neko-updater-tray)
-set(__QT_DEPLOY_TARGET_neko-updater-tray_TYPE EXECUTABLE)
--- neko-updater-tray/build/CMakeFiles/neko-updater-tray.dir/neko-updater-tray_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/neko-updater-tray.dir/neko-updater-tray_autogen/mocs_compilation.cpp.o: \
- /home/anomali/STORAGE/Proyectos/Neko-Update/neko-updater-tray/build/neko-updater-tray_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
